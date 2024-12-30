54b08c82813ea374d600f8f739b6500d1a06331c
Mikhail <mikhail.epatko@gmail.com>
2024-12-30 11:26:09 +0300

    Updated template

--- src/main/kotlin/ru/epatko/template/Application.kt
+++ src/main/kotlin/ru/epatko/template/Application.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class TemplateApplication
+class Application
 
 fun main(args: Array<String>) {
-	runApplication<TemplateApplication>(*args)
+	runApplication<Application>(*args)
 }
